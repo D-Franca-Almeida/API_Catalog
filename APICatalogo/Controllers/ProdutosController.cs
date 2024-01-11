35eb84a3fc59607cc104ff766b4a4fa533a1709c
Daniel França Almeida <daniel.franca0512@gmail.com>
2024-01-11 18:37:48 -0300

    Criação dos EndPoints da classe Categoria, entretanto esta com problema no Json

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -21,18 +22,18 @@ namespace APICatalogo.Controllers
         public ActionResult<IEnumerable<Produto>> Get()
         {
             var produtos = _context.Produtos.ToList();
-            if(produtos is null)
+            if (produtos is null)
             {
                 return NotFound("Produtos não encontrados...");  // Retorna o erro quando os dados forem nulos
             }
             return produtos;
         }
 
-        [HttpGet("{id:int}", Name="ObterProduto")] 
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produtos = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if(produtos is null)
+            if (produtos is null)
             {
                 return NotFound();
             }
@@ -45,7 +46,7 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult Post(Produto produto)
         {
-            if(produto is null)
+            if (produto is null)
             {
                 return BadRequest();
             }
@@ -54,27 +55,48 @@ namespace APICatalogo.Controllers
 
             //Retorna o 201 created no header location
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
-            
+
         }
 
 
 
         #endregion
 
-
         #region PUT
 
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId) //Garanti alterar o produto correto
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified; //Entidade precisa ser persistida
+            _context.SaveChanges();
+
+            return Ok(produto); // retorna 200 e retorna também os dados do produto. 
+        }
+
 
         #endregion
-
 
         #region DELETE
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
 
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges(); //Salvando as alterações do contexto. 
+
+            return Ok(produto);
+        }
         #endregion
-
-
-
-
-
     }
 }
+
